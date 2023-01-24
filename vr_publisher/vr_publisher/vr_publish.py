754b9c61dbf87ccfe57653ccb77a79f3892b2d3a
09ubberboy90 <2330834a@student.gla.ac.uk>
2023-01-24 15:03:45 +0000

    Try hand rotation based on headset. Not working

--- vr_publisher/vr_publisher/vr_publish.py
+++ vr_publisher/vr_publisher/vr_publish.py
@@ -1,32 +1,32 @@
-#BSD 3-Clause License
+# BSD 3-Clause License
 #
-#Copyright (c) 2021, Florent Audonnet
-#All rights reserved.
+# Copyright (c) 2021, Florent Audonnet
+# All rights reserved.
 #
-#Redistribution and use in source and binary forms, with or without
-#modification, are permitted provided that the following conditions are met:
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 #
-#1. Redistributions of source code must retain the above copyright notice, this
+# 1. Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 #
-#2. Redistributions in binary form must reproduce the above copyright notice,
+# 2. Redistributions in binary form must reproduce the above copyright notice,
 #   this list of conditions and the following disclaimer in the documentation
 #   and/or other materials provided with the distribution.
 #
-#3. Neither the name of the copyright holder nor the names of its
+# 3. Neither the name of the copyright holder nor the names of its
 #   contributors may be used to endorse or promote products derived from
 #   this software without specific prior written permission.
 #
-#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-#FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-#DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-#CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-#OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # https: // gist.github.com/awesomebytes/75daab3adb62b331f21ecf3a03b3ab46
 import math
 import sys
@@ -51,7 +51,8 @@ def convert_to_euler(pose_mat):
     x = pose_mat[0][3]
     y = pose_mat[1][3]
     z = pose_mat[2][3]
-    return [[x,y,z],[yaw,pitch,roll]]
+    return [[x, y, z], [yaw, pitch, roll]]
+
 
 def convert_to_quaternion(pose_mat):
     # Per issue #2, adding a abs() so that sqrt only results in real numbers
@@ -59,19 +60,24 @@ def convert_to_quaternion(pose_mat):
     # r_x = (pose_mat[2][1]-pose_mat[1][2])/(4*r_w)
     # r_y = (pose_mat[0][2]-pose_mat[2][0])/(4*r_w)
     # r_z = (pose_mat[1][0]-pose_mat[0][1])/(4*r_w)
-    
-    q_w = math.sqrt(max(0, 1 + pose_mat[0][0] + pose_mat[1][1] + pose_mat[2][2])) / 2;
-    q_x = math.sqrt(max(0, 1 + pose_mat[0][0] - pose_mat[1][1] - pose_mat[2][2])) / 2;
-    q_y = math.sqrt(max(0, 1 - pose_mat[0][0] + pose_mat[1][1] - pose_mat[2][2])) / 2;
-    q_z = math.sqrt(max(0, 1 - pose_mat[0][0] - pose_mat[1][1] + pose_mat[2][2])) / 2;
-    q_x = math.copysign(q_x, pose_mat[2][1] - pose_mat[1][2]);
-    q_y = math.copysign(q_y, pose_mat[0][2] - pose_mat[2][0]);
-    q_z = math.copysign(q_z, pose_mat[1][0] - pose_mat[0][1]);
+
+    q_w = math.sqrt(
+        max(0, 1 + pose_mat[0][0] + pose_mat[1][1] + pose_mat[2][2])) / 2
+    q_x = math.sqrt(
+        max(0, 1 + pose_mat[0][0] - pose_mat[1][1] - pose_mat[2][2])) / 2
+    q_y = math.sqrt(
+        max(0, 1 - pose_mat[0][0] + pose_mat[1][1] - pose_mat[2][2])) / 2
+    q_z = math.sqrt(
+        max(0, 1 - pose_mat[0][0] - pose_mat[1][1] + pose_mat[2][2])) / 2
+    q_x = math.copysign(q_x, pose_mat[2][1] - pose_mat[1][2])
+    q_y = math.copysign(q_y, pose_mat[0][2] - pose_mat[2][0])
+    q_z = math.copysign(q_z, pose_mat[1][0] - pose_mat[0][1])
 
     x = pose_mat[0][3]
     y = pose_mat[1][3]
     z = pose_mat[2][3]
-    return [[x,y,z],[q_w,q_x,q_y,q_z]]
+    return [[x, y, z], [q_w, q_x, q_y, q_z]]
+
 
 def from_controller_state_to_dict(pControllerState):
     # docs: https://github.com/ValveSoftware/openvr/wiki/IVRSystem::GetControllerState
@@ -79,19 +85,19 @@ def from_controller_state_to_dict(pControllerState):
     controller_state['unPacketNum'] = pControllerState.unPacketNum
     # on trigger .y is always 0.0 says the docs
     controller_state['trigger'] = pControllerState.rAxis[3].x
-    try:    
+    try:
         print(f"1 : {pControllerState.rAxis0} {pControllerState.rAxis0}")
     except:
         pass
-    try:    
+    try:
         print(f"2 : {pControllerState.rAxis1.x} {pControllerState.rAxis1.y}")
     except:
         pass
-    try:    
+    try:
         print(f"3 : {pControllerState.rAxis2.x} {pControllerState.rAxis2.y}")
     except:
         pass
-    try:    
+    try:
         print(f"4 : {pControllerState.rAxis3.x} {pControllerState.rAxis3.y}")
     except:
         pass
@@ -107,21 +113,24 @@ def from_controller_state_to_dict(pControllerState):
     controller_state['ulButtonTouched'] = pControllerState.ulButtonTouched
     # To make easier to understand what is going on
     # Second bit marks menu button
-    controller_state['menu_button'] = bool(pControllerState.ulButtonPressed >> 1 & 1)
+    controller_state['menu_button'] = bool(
+        pControllerState.ulButtonPressed >> 1 & 1)
     # 32 bit marks trackpad
-    controller_state['trackpad_pressed'] = bool(pControllerState.ulButtonPressed >> 32 & 1)
-    controller_state['trackpad_touched'] = bool(pControllerState.ulButtonTouched >> 32 & 1)
+    controller_state['trackpad_pressed'] = bool(
+        pControllerState.ulButtonPressed >> 32 & 1)
+    controller_state['trackpad_touched'] = bool(
+        pControllerState.ulButtonTouched >> 32 & 1)
     # third bit marks grip button
-    controller_state['grip_button'] = bool(pControllerState.ulButtonPressed >> 2 & 1)
+    controller_state['grip_button'] = bool(
+        pControllerState.ulButtonPressed >> 2 & 1)
     # System button can't be read, if you press it
     # the controllers stop reporting
     return controller_state
 
 
-
 class VrPublisher(Node):
 
-    def __init__(self, openvr_system:openvr.IVRSystem):
+    def __init__(self, openvr_system: openvr.IVRSystem):
         super().__init__('vr_publisher')
         timer_period = 0.05
         self.timer = self.create_timer(timer_period, self.timer_callback)
@@ -132,14 +141,16 @@ class VrPublisher(Node):
         self.prev_time = datetime.now()
         self.point = {}
         self.rot = {}
-        self.vel = defaultdict(lambda: np.zeros((5,3)))
-        self.ang_vel = defaultdict(lambda: np.zeros((5,3)))
+        self.vel = defaultdict(lambda: np.zeros((5, 3)))
+        self.ang_vel = defaultdict(lambda: np.zeros((5, 3)))
         self.hmd_pose = Pose()
+        self.hmd_rot = pyq.Quaternion()
+
         self.last_hmd_pose = None
         self.controller_state = {}
         self.tracking_enabled = defaultdict(lambda: True)
 
-    def extract_pose(self, tracked_device:openvr.TrackedDevicePose_t, name:str) -> Tuple[Pose, Twist]:
+    def extract_pose(self, tracked_device: openvr.TrackedDevicePose_t, name: str) -> Tuple[Pose, Twist]:
         point = Point()
         velocity = Vector3()
         ang_velocity = Vector3()
@@ -154,36 +165,39 @@ class VrPublisher(Node):
         point.z = pose[0][2] - self.hmd_pose.position.z
 
         # if self.point.get(name, None) is not None:
-        self.vel[name] = np.roll(self.vel[name],1,axis=0)
-        self.vel[name][-1] = [round(num,2) for num in tracked_device.vVelocity[0:3]]
-        self.ang_vel[name] = np.roll(self.ang_vel[name],1,axis=0)
-        self.ang_vel[name][-1] = [round(num,2) for num in tracked_device.vAngularVelocity[0:3]]
-        mean = np.mean(self.vel[name],0)
-        ang_mean = np.mean(self.ang_vel[name],0)
+        self.vel[name] = np.roll(self.vel[name], 1, axis=0)
+        self.vel[name][-1] = [round(num, 2)
+                              for num in tracked_device.vVelocity[0:3]]
+        self.ang_vel[name] = np.roll(self.ang_vel[name], 1, axis=0)
+        self.ang_vel[name][-1] = [round(num, 2)
+                                  for num in tracked_device.vAngularVelocity[0:3]]
+        mean = np.mean(self.vel[name], 0)
+        ang_mean = np.mean(self.ang_vel[name], 0)
         velocity.x = -mean[2]
-        velocity.y = mean[1] 
-        velocity.z = -mean[0] 
-        ang_velocity.x = ang_mean[2] 
-        ang_velocity.y = ang_mean[1] 
-        ang_velocity.z = ang_mean[0] 
+        velocity.y = mean[1]
+        velocity.z = -mean[0]
+        ang_velocity.x = ang_mean[2]
+        ang_velocity.y = ang_mean[1]
+        ang_velocity.z = ang_mean[0]
 
         rot = Quaternion()
 
         q1 = pyq.Quaternion(pose[1])
 
+        hmd_msg = Pose()
+        if name == "hmd":
+            self.hmd_rot = q1
 
         rot.w = q1.w
         rot.x = q1.x
         rot.y = q1.y
         rot.z = q1.z
-        
-        hmd_msg = Pose()
+
         if name == "hmd":
+            self.hmd_pose = hmd_msg
             hmd_msg.orientation = rot
             hmd_msg.position = orig_point
-            self.hmd_pose = hmd_msg
 
-        self.rot[name] = q1
         pose_msg = Pose()
         pose_msg.orientation = rot
         pose_msg.position = point
@@ -197,8 +211,6 @@ class VrPublisher(Node):
     def timer_callback(self):
         self.poses = self.system.getDeviceToAbsoluteTrackingPose(
             openvr.TrackingUniverseStanding, 0, self.poses)
-
-        
 
         ########
         # # ETrackedDeviceClass = ENUM_TYPE
@@ -219,33 +231,30 @@ class VrPublisher(Node):
                 continue
             if self.system.getTrackedDeviceClass(idx) == 1:
                 self.devices["hmd"] = (controller, idx)
-            elif self.system.getTrackedDeviceClass(idx) == 2 and (self.devices.get("left_hand",None) is None or self.devices.get("right_hand", None) is None):
+            elif self.system.getTrackedDeviceClass(idx) == 2 and (self.devices.get("left_hand", None) is None or self.devices.get("right_hand", None) is None):
                 controller_role = self.system.getControllerRoleForTrackedDeviceIndex(
                     idx)
-                    
+
                 if controller_role == 1:
                     self.devices["left_hand"] = (controller, idx)
                 if controller_role == 2:
                     self.devices["right_hand"] = (controller, idx)
             elif self.system.getTrackedDeviceClass(idx) == 3:
-                    self.devices[f"tracker_{idx}"]= (controller, idx)
+                self.devices[f"tracker_{idx}"] = (controller, idx)
 
         for name, (el, idx) in self.devices.items():
-            if "hand" in name:
-                result, pControllerState = self.system.getControllerState(
-                    idx)
-                if result and name:
-                    self.controller_state[name] = from_controller_state_to_dict(pControllerState)
-                    # if name == "right_hand":
-                    #     print(self.controller_state)
+            result, pControllerState = self.system.getControllerState(
+                idx)
+            if result and name:
+                self.controller_state[name] = from_controller_state_to_dict(
+                    pControllerState)
+                if "hand" in name:
                     tmp_name = f"{name}/trigger"
                     msg = Bool()
-                    msg.data = self.controller_state[name]["ulButtonPressed"] >= 8589934592 # At least TRigger pressed
+                    # At least TRigger pressed
+                    msg.data = self.controller_state[name]["ulButtonPressed"] >= 8589934592
                     self.publish(tmp_name, msg, Bool)
-                    
 
-        
-        
             pose_msg, vel_msg = self.extract_pose(el, name)
 
             if name != "hmd":
@@ -254,9 +263,14 @@ class VrPublisher(Node):
                         self.tracking_enabled[name] = not self.tracking_enabled[name]
                 except:
                     pass
-                self.controller_state[name+"_prev"] = self.controller_state[name]
+                try:
+                    self.controller_state[name +
+                                          "_prev"] = self.controller_state[name]
+                except:
+                    pass
+
             if not self.tracking_enabled[name]:
-                pose_msg.position.x = 3001.0 ## we want to disable tracking if the trackpad is pressed
+                pose_msg.position.x = 3001.0  # we want to disable tracking if the trackpad is pressed
 
             name = f"{name}/pose"
 
